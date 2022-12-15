61ae6307150ff0e879cdae1be206c700d59933a9
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2022-12-15 02:08:25 +0100

    Create src.cpp

---
+++ src.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+// Function that performs PCA on the input matrix
+MatrixXd pca(const MatrixXd& X) {
+  // Center the data by subtracting the mean of each column
+  MatrixXd X_centered = X.rowwise() - X.colwise().mean();
+
+  // Compute the covariance matrix
+  MatrixXd cov = X_centered.transpose() * X_centered / (X.rows() - 1);
+
+  // Compute the eigenvectors and eigenvalues of the covariance matrix
+  Eigen::SelfAdjointEigenSolver<MatrixXd> eigenSolver(cov);
+  MatrixXd eigenvectors = eigenSolver.eigenvectors();
+  MatrixXd eigenvalues = eigenSolver.eigenvalues();
+
+  // Return the matrix of eigenvectors, sorted by eigenvalue in descending order
+  return eigenvectors.colwise().reverse();
+}
+
+// Function that performs t-SNE on the input matrix
+MatrixXd tsne(const MatrixXd& X) {
+  // Initialize the matrix of output coordinates with random values
+  MatrixXd Y = MatrixXd::Random(X.rows(), 2);
+
+  // Perform gradient descent to optimize the output coordinates
+  // The number of iterations and the learning rate can be adjusted
+  int numIterations = 1000;
+  double learningRate = 10.0;
+  for (int i = 0; i < numIterations; i++) {
+    // Compute the pairwise distances between the input data points
+    MatrixXd distances = (X.rowwise() - X.colwise().mean()).rowwise().norm().eval();
+
+    // Compute the pairwise affinities between the output coordinates
+    MatrixXd affinities = (Y.rowwise() - Y.colwise().mean()).rowwise().norm().eval();
+
+    // Compute the gradient of the KL divergence with respect to the output coordinates
+    MatrixXd gradients = ((distances - affinities) / distances.array().square()).rowwise().mean().eval();
+    Y -= learningRate * gradients;
+  }
+
+  // Return the matrix of output coordinates
+  return Y;
+}
+
+int main() {
+  // Define a matrix of input data
+  MatrixXd X(5, 3);
+  X << 1, 2, 3,
+       2, 3, 4,
+       3, 4, 5,
+       4, 5, 6,
+       5, 6, 7;
+
+  // Perform PCA on the input data
+  MatrixXd X_pca = pca(X);
+  std::cout << "PCA output:\n" << X_pca << std::endl;
+
+  // Perform t-SNE on the input data
+  MatrixXd X_tsne = tsne(X);
+  std
